8a1093c796fc2e8c9e3891c1e154c67f48d58945
timchae <timchae2@gmail.com>
2021-07-16 15:58:48 +0900

    프로젝트 생성

---
+++ src/test/java/com/clone/pinterest/PinterestCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clone.pinterest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PinterestCloneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
