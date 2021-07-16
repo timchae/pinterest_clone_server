258ce6fbdd9c6175ee23d6d249ee7ab4be5aa694
cactus-seok <sangpil95@gmail.com>
2021-07-16 17:49:08 +0900

    UserController 설계
    로그인, 회원가입 설계

---
+++ src/main/java/com/clone/pinterest/controller/UserController.java
@@ -0,0 +1,27 @@
+package com.clone.pinterest.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    //로그인
+    @PostMapping("/user/login")
+    public String login(@RequestBody UserRequestDto userRequestDto){
+        return userService.createToken(userRequestDto);
+    }
+
+    //회원가입
+    @PostMapping("/user/register")
+    public void userRegister(@RequestBody SignupRequestDto signupRequestDto){
+        userService.registerUser(signupRequestDto);
+    }
+
+    
+}
