c0aa1fdd4e9047ad38a4a1b711980435d8f98b91
박강희 <kh910109@naver.com>
2021-07-22 17:43:42 +0900

    response 수정 검색 기능 수정

--- src/main/java/com/clone/pinterest/service/PinService.java
+++ src/main/java/com/clone/pinterest/service/PinService.java
@@ -4,15 +4,12 @@ import com.clone.pinterest.domain.Board;
 import com.clone.pinterest.domain.Pin;
 import com.clone.pinterest.domain.User;
 import com.clone.pinterest.dto.request.PinRequestDto;
-import com.clone.pinterest.dto.response.MyPinResponseDto;
 import com.clone.pinterest.dto.response.PinResponseDto;
-import com.clone.pinterest.dto.response.PinSearchResponseDto;
 import com.clone.pinterest.exception.ApiRequestException;
 import com.clone.pinterest.repository.BoardRepository;
 import com.clone.pinterest.repository.CommentsRepository;
 import com.clone.pinterest.repository.LikenRepository;
 import com.clone.pinterest.repository.PinRepository;
-import io.swagger.annotations.Api;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -21,7 +18,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -106,35 +102,23 @@ public class PinService {
     }
 
     // pin 특정 조회(내가 쓴 핀)
-    public List<MyPinResponseDto> readMyPin(User user) {
+    public List<PinResponseDto> readMyPin(User user) {
         List<Pin> pinList = pinRepository.findAllByUser(user);
-        List<MyPinResponseDto> result = pinList.stream()
-                .map(pin -> new MyPinResponseDto(pin))
+        List<PinResponseDto> result = pinList.stream()
+                .map(pin -> new PinResponseDto(pin))
                 .collect(Collectors.toList());
         return result;
     }
 
     // pin 검색 (pinTitle)
-    public List<PinSearchResponseDto> readSearchPin(String keyword) {
-        List<Pin> pins = pinRepository.findByPinTitleContaining(keyword);
-        List<PinSearchResponseDto> pinSearchResponseDtos = new ArrayList<>();
-
-        for (Pin pin : pins ) {
-            PinSearchResponseDto pinSearchResponseDto = new PinSearchResponseDto (
-                    pin.getPinId(),
-                    pin.getUser(),
-                    pin.getPinTitle(),
-                    pin.getPinContent(),
-                    pin.getPinImage(),
-                    pin.getPinUrl(),
-                    pin.getCreatedAt()
-            );
-
-            pinSearchResponseDtos.add(pinSearchResponseDto);
-        }
-
-        return pinSearchResponseDtos;
+    public List<PinResponseDto> readSearchPin(String keyword) {
+        List<Pin> pinList = pinRepository.findByPinTitleContaining(keyword);
+        List<PinResponseDto> result = pinList.stream()
+                .map(pin -> new PinResponseDto(pin))
+                .collect(Collectors.toList());
+        return result;
     }
+
     @Transactional
     public Pin addpin(Long id, Long boardId, User user) {
         Pin pin = pinRepository.findById(id).orElseThrow(
